d8b9758f0ca4c74ef5004dcda4c348ea0cc0a350
Mark Conn <misterconn@gmail.com>
2016-11-03 15:11:52 +0000

    ex4

--- script/app.js
+++ script/app.js
@@ -1,1 +1,18 @@
-/* Write your code here! */
+var $;
+$('#signup-form').on('submit', function(event) {
+    var passLength = $(event.currentTarget[1]).val().length;
+    var pass1 = $(event.currentTarget[1]).val();
+    var pass2 = $(event.currentTarget[2]).val();
+    if((passLength >= 10) && (pass1 === pass2)) {
+      console.log("success")
+    } else {
+      event.preventDefault();
+      if(passLength < 10) alert("your password isnt long enough");
+      else {
+        alert("your passwords do not match")
+      }
+      
+    }
+});
+
+
