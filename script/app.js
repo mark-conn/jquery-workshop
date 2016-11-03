096ce31436da4a15d95b0c2ab9b732b331b1026a
Mark Conn <misterconn@gmail.com>
2016-11-03 15:44:12 +0000

    ex5

--- script/app.js
+++ script/app.js
@@ -1,1 +1,8 @@
 /* Write your code here! */
+jQuery.getJSON('https://api.wheretheiss.at/v1/satellites/25544').then(
+    function(res) {
+        var issLon = res.longitude;
+        var issLat = res.latitude;
+        $('.title').after(`<div class="iss"><h3>ISS POSITION</h3><p>LAT: ${issLat}</p><p>LON: ${issLon}</p></div>`);
+    }
+)
